efc08e4bd89a3dda3114105698bec8851c037362
djalmajr <djalmajr@gmail.com>
2018-11-06 21:49:56 -0300

    feat: validates start envs

--- bin/start.js
+++ bin/start.js
@@ -1,9 +1,16 @@
 #! /usr/bin/env node
 var shell = require("shelljs");
 
-const envs = process.argv
-  .slice(2)
-  .concat("NODE_ENV=development")
-  .join(" ");
+const envs = ["NODE_ENV=development"];
 
-shell.exec(`cross-env ${envs} webpack-dev-server --mode=development`);
+process.argv.forEach(arg => {
+  const parts = arg.split("=");
+
+  if (parts[0] === "--hot") {
+    envs.push("HOT=true");
+  } else if (parts.length === 2 && parts[1]) {
+    envs.push(arg);
+  }
+});
+
+shell.exec(`cross-env ${envs.join(" ")} webpack-dev-server --mode=development`);
